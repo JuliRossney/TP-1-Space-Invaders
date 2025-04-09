1063b93e3b2e88ca04996058a402dde78b3cd3f3
nickk <ifalcone@alumno.huergo.edu.ar>
2025-04-09 10:47:50 -0300

    prueba

--- borrador.py
+++ borrador.py
@@ -1,10 +1,9 @@
 import pygame
 import sys
-
+print("holaa")
 pygame.init()
 
 ancho = 600
 altura = 1000
 pantalla = pygame.display.set_mode((ancho, altura))
 pygame.display.set_caption("Space Invaders Juli")
-
